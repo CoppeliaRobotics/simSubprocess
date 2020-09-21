41fc378c3c576cbc6e9812847764f4f94538637c
Federico Ferri <federico.ferri.it@gmail.com>
2020-09-21 16:19:40 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# Subprocess plugin for CoppeliaSim
+
+### Compiling
+
+1. Install required packages for [libPlugin](https://github.com/CoppeliaRobotics/libPlugin): see libPlugin's [README](external/libPlugin/README.md)
+2. Checkout and compile
+```text
+$ git clone --recursive https://github.com/CoppeliaRobotics/simExtSubprocessgit
+$ cmake .
+$ cmake --build .
+```
---
+++ plugin.cpp
@@ -0,0 +1,47 @@
+#include <string>
+#include <vector>
+#include <boost/process.hpp>
+
+#include "simPlusPlus/Plugin.h"
+#include "plugin.h"
+#include "stubs.h"
+
+using std::string;
+using std::vector;
+using std::runtime_error;
+namespace bp = boost::process;
+
+class Plugin : public sim::Plugin
+{
+public:
+    void onStart()
+    {
+        if(!registerScriptStuff())
+            throw runtime_error("failed to register script stuff");
+
+        setExtVersion("Subprocess Plugin");
+        setBuildDate(BUILD_DATE);
+    }
+
+    void exec(exec_in *in, exec_out *out)
+    {
+        bp::opstream sin;
+        bp::ipstream sout;
+        boost::filesystem::path program;
+        if(in->useSearchPath)
+            program = bp::search_path(in->programPath);
+        else
+            program = in->programPath;
+        bp::child child(program, in->args, bp::std_in = sin, bp::std_out > sout);
+        sin << in->stdin;
+        sin.flush();
+        sin.pipe().close();
+        child.wait();
+        out->exitCode = child.exit_code();
+        std::string s(std::istreambuf_iterator<char>(sout), {});
+        out->stdout = s;
+    }
+};
+
+SIM_PLUGIN(PLUGIN_NAME, PLUGIN_VERSION, Plugin)
+#include "stubsPlusPlus.cpp"
