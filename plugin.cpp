45d15ae437663879b4251dbc73626d49d8ce0327
Federico Ferri <federico.ferri.it@gmail.com>
2021-12-14 17:02:30 +0100

    use QProcess

--- plugin.cpp
+++ plugin.cpp
@@ -1,7 +1,12 @@
 #include <string>
 #include <vector>
-#include <boost/process.hpp>
-#include <boost/process/async.hpp>
+
+#include <QByteArray>
+#include <QString>
+#include <QStringList>
+#include <QDir>
+#include <QFileInfo>
+#include <QProcess>
 
 #include "simPlusPlus/Plugin.h"
 #include "simPlusPlus/Handle.h"
@@ -13,15 +18,7 @@ using std::string;
 using std::vector;
 using std::runtime_error;
 
-namespace bp = boost::process;
-
-struct Child {
-    bp::child *child;
-    std::future<string> out;
-    std::future<string> err;
-};
-
-template<> string sim::Handle<Child>::tag() { return "simSubprocess.child"; }
+template<> string sim::Handle<QProcess>::tag() { return "simSubprocess.child"; }
 
 class Plugin : public sim::Plugin
 {
@@ -35,84 +32,114 @@ public:
         setBuildDate(BUILD_DATE);
     }
 
-    void onInstancePass(const sim::InstancePassFlags &flags)
-    {
-        ioc.run_for(std::chrono::milliseconds{2});
-    }
-
     void onScriptStateDestroyed(int scriptID)
     {
         for(auto c : handles.find(scriptID))
         {
-            c->child->wait();
-            delete c->child;
             delete handles.remove(c);
         }
     }
 
     template<class T>
-    boost::filesystem::path getProgram(const T &in)
+    QString getProgram(const T &in)
     {
-        boost::filesystem::path program;
+        QString program = QString::fromStdString(in->programPath);
         if(in->useSearchPath)
-            program = bp::search_path(in->programPath);
-        else
-            program = in->programPath;
+        {
+            QString path = qEnvironmentVariable("PATH");
+            if(!path.isNull())
+            {
+                QStringList dirList(path.split(QDir::listSeparator()));
+                QStringList extList;
+                extList << ""
+#ifdef _MSC_VER
+                    << ".exe"
+#endif // _MSC_VER
+                    ;
+                for(const auto &dir : dirList)
+                {
+                    QDir d(dir);
+                    for(const auto &ext : extList)
+                    {
+                        QFileInfo f(d, program + ext);
+                        if(f.exists())
+                        {
+                            return f.absoluteFilePath();
+                        }
+                    }
+                }
+            }
+        }
         return program;
+    }
+
+    template<class T>
+    QStringList getArguments(const T &in)
+    {
+        QStringList ret;
+        for(const auto &arg : in->args)
+            ret << QString::fromStdString(arg);
+        return ret;
     }
 
     void exec(exec_in *in, exec_out *out)
     {
-        bp::opstream sin;
-        bp::ipstream sout;
-        bp::child child(getProgram(in), in->args, bp::std_in = sin, bp::std_out > sout);
-        sin << in->input;
-        sin.flush();
-        sin.pipe().close();
-        child.wait();
-        out->exitCode = child.exit_code();
-        std::string s(std::istreambuf_iterator<char>(sout), {});
-        out->output = s;
+        QProcess *process = new QProcess();
+        process->start(getProgram(in), getArguments(in));
+        if(!process->waitForStarted(-1))
+            throw std::runtime_error("waitForStarted timed out");
+        process->write(QByteArray(in->input.c_str(), in->input.length()));
+        process->closeWriteChannel();
+        if(!process->waitForFinished(-1))
+            throw std::runtime_error("waitForFinished timed out");
+        QByteArray outData = process->readAllStandardOutput();
+        out->output = std::string(outData.constData(), outData.length());
+        if(process->exitStatus() == QProcess::NormalExit)
+            out->exitCode = process->exitCode();
+        else if(process->exitStatus() == QProcess::CrashExit)
+            throw std::runtime_error("the process crashed");
+        else
+            throw std::runtime_error("unknown process exit status");
+        delete process;
     }
 
     void execAsync(execAsync_in *in, execAsync_out *out)
     {
-        auto c = new Child;
-        c->child = new bp::child(getProgram(in), in->args, bp::std_out > c->out, bp::std_err > c->err, ioc);
-        out->handle = handles.add(c, in->_.scriptID);
+        QProcess *process = new QProcess();
+        process->start(getProgram(in), getArguments(in));
+        if(!process->waitForStarted(-1))
+            throw std::runtime_error("waitForStarted timed out");
+        out->handle = handles.add(process, in->_.scriptID);
     }
 
     void isRunning(isRunning_in *in, isRunning_out *out)
     {
-        auto c = handles.get(in->handle);
-        out->running = c->child->running();
+        auto process = handles.get(in->handle);
+        out->running = process->state() == QProcess::Running;
     }
 
     void wait(wait_in *in, wait_out *out)
     {
-        auto c = handles.get(in->handle);
-        auto t = std::chrono::milliseconds{long(1000 * in->timeout)};
-        if(c->child->wait_for(t))
-        {
-            out->exitCode = c->child->exit_code();
-        }
+        auto process = handles.get(in->handle);
+        if(!process->waitForFinished(int(1000 * in->timeout)))
+            return;
+        if(process->exitStatus() == QProcess::NormalExit)
+            out->exitCode = process->exitCode();
+        else if(process->exitStatus() == QProcess::CrashExit)
+            throw std::runtime_error("the process crashed");
+        else
+            throw std::runtime_error("unknown process exit status");
     }
 
     void terminate(terminate_in *in, terminate_out *out)
     {
-        auto c = handles.get(in->handle);
-        c->child->terminate();
-#if 0 // this freezes the app (on macOS at least)
-        c->child->wait(); //to avoid a zombie process & get the exit code
-        out->exitCode = c->child->exit_code();
-#endif
-        delete c->child;
-        delete handles.remove(c);
+        auto process = handles.get(in->handle);
+        process->kill();
+        delete handles.remove(process);
     }
 
 private:
-    sim::Handles<Child> handles;
-    boost::asio::io_context ioc;
+    sim::Handles<QProcess> handles;
 };
 
 SIM_PLUGIN(PLUGIN_NAME, PLUGIN_VERSION, Plugin)
